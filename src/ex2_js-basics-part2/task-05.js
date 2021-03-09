e4fb4604913d9a4ee0a297fbdc2f891565705057
Valeripetrukhina <79864824+Valeripetrukhina@users.noreply.github.com>
2021-03-09 14:25:31 +0300

    Delete task-05.js

--- src/ex2_js-basics-part2/task-05.js
+++
@@ -1,7 +0,0 @@
-function sumPositive(positiveNum) {
-  let positive = positiveNum.filter((item) => item > 0);
-  return positive.reduce(
-    (previousValue, currentValue) => previousValue + currentValue
-  );
-}
-module.exports = sumPositive;
