eaeb78cf1735e324ed5ca8219e9bdb9bb8ecec81
Valeripetrukhina <79864824+Valeripetrukhina@users.noreply.github.com>
2021-03-09 15:32:46 +0300

    Delete task-01.js

--- src/ex2_js-basics-part2/task-01.js
+++
@@ -1,9 +0,0 @@
-function argumentType(value) {
-  if (Number.isNaN(value)) {
-    return "undefined";
-  } else if (typeof value !== "number" && typeof value !== "string") {
-    return "undefined";
-  }
-  return typeof value;
-}
-module.exports = argumentType;
