7590af313044d51f7e2b648559dc77140b01985c
Valeripetrukhina <79864824+Valeripetrukhina@users.noreply.github.com>
2021-03-09 15:37:41 +0300

    add task 1

--- src/ex2_js-basics-part2/task-01.js
+++ src/ex2_js-basics-part2/task-01.js
@@ -1,9 +1,12 @@
 function argumentType(value) {
   if (Number.isNaN(value)) {
-    return "undefined";
-  } else if (typeof value !== "number" && typeof value !== "string") {
-    return "undefined";
-  }
-  return typeof value;
+    return("undefined");
+  } else if (typeof value === 'number') {
+    return("number");
+  } else if (typeof value === 'string') {
+    return("string");
+  } 
+    return(undefined);
+  
 }
 module.exports = argumentType;
