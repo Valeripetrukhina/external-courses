06c6df71dc542b103743f90d14094285495a1b07
Valeripetrukhina <79864824+Valeripetrukhina@users.noreply.github.com>
2021-03-19 18:46:37 +0300

    Delete task-10.js

--- src/ex4_js-objects-part2/task-10.js
+++
@@ -1,8 +0,0 @@
-function invertedLine(str) {
-  let strArr = str.split('');
-  let invertArray = strArr.reverse();
-  let arrStr = invertArray.join('');
-  return arrStr;
-}
-
-module.exports = invertedLine;
