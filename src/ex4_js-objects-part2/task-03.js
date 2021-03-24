90b85a9e312a0ca854ba167276b51ec3ad2bbe0c
Valeripetrukhina <79864824+Valeripetrukhina@users.noreply.github.com>
2021-03-24 23:35:43 +0300

    add task4 (03)

--- src/ex4_js-objects-part2/task-03.js
+++ src/ex4_js-objects-part2/task-03.js
@@ -1,4 +1,11 @@
-function deleteSpase (str){
-    return str.trim()
- }
+function deleteSpase(str) {
+    if(str[0] === ' ' && str[str.length-1] === ' ') {
+        return str.substring(1, str.length-1)
+    } if(str[0] === ' ') {
+        return str.substring(1, str.length)
+    } if(str[str.length-1] === ' ') {
+        return str.substring(0, str.length-1)
+    } return str
+};
+
  module.exports = deleteSpase
