bfab1dc137f8a4e0b28e54ef07af94ca9b227f06
Valeripetrukhina <79864824+Valeripetrukhina@users.noreply.github.com>
2021-03-29 11:45:01 +0300

    add task5

---
+++ src/ex5_js-functions/task.js
@@ -0,0 +1,59 @@
+function Calculator() {
+  this.result = 0;
+
+  this.add = (num) => {
+    if (!num) {
+      this.result = this.result + 0;
+    } else {
+      this.result = this.result + num;
+    }
+
+    return this.add;
+  };
+  this.subtract = (num) => {
+    if (!num) {
+      this.result = this.result + 0;
+    } else {
+      this.result = this.result - num;
+    }
+
+    return this.subtract;
+  };
+  this.divide = (num) => {
+    if (!num) {
+      this.result = this.result + 0;
+    } else if (this.result !== 0) {
+      this.result = this.result / num;
+    } else {
+      this.result = (this.result + 0) / num;
+    }
+
+    return this.divide;
+  };
+  this.multiply = (num) => {
+    if (!num) {
+      this.result = this.result + 0;
+    } else if (this.result !== 0) {
+      this.result = this.result * num;
+    } else {
+      this.result = (this.result + 1) * num;
+    }
+
+    return this.multiply;
+  };
+  this.getResult = (num) => {
+    if (!num) {
+      this.result = this.result;
+    }
+    return this.result;
+  };
+  this.reset = (num) => {
+    this.result = this.result * 0;
+
+    return this.reset;
+  };
+}
+
+let calculator = new Calculator();
+
+module.exports = calculator;
